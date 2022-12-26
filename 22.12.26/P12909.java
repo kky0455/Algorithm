716c831a24e4d474d13a74d4c4766afe2cac9402
김광용 <veta6287@naver.com>
2022-12-26 23:19:09 +0900

    22.12.26

---
+++ 22.12.26/P12909.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class P12909 {
+    boolean solution(String s) {
+        boolean answer = false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
